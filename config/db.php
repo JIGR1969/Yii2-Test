a2f35e2f545638fcb9444ee2f0b644aa7083f677
root <james@grantzone.net>
2015-12-21 22:33:14 +0000

    Initial GIT submission.

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'stucra8h9T',
+    'charset' => 'utf8',
+];
